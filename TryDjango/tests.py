24d21d5d4a67a395b2d6b2e50587ab36becde366
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2022-01-16 16:04:09 +0330

    Automated Test Basics

---
+++ TryDjango/tests.py
@@ -0,0 +1,22 @@
+import os
+from django.test import TestCase
+# or do this for getting secret key
+from django.conf import settings
+from django.contrib.auth.password_validation import validate_password
+
+
+class TryDjangoConfigTest(TestCase):
+    # all the methods have to start with test_<anything> 
+    # Checkout python unittest > TestCase for more info
+    def test_secret_key_strength(self):
+        # get the secret key
+        SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+        # another way: SECRET_KEY = settings.SECRET_KEY
+        # how to make sure the password is good?
+        try:
+            is_strong = validate_password(SECRET_KEY)
+        except Exception as e:
+            msg = f'Bad SECRET_KEY {e.messages}'
+            self.fail(msg)
+
+    
